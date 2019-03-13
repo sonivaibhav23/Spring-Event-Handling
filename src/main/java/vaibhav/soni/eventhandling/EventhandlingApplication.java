ba1e5e1b3bcb44a70bc0024af24c8df317338d3a
TKMA08U <vaibhav.soni@kohls.com>
2019-03-13 16:26:46 +0530

    [Add] - added structure of project

---
+++ src/main/java/vaibhav/soni/eventhandling/EventhandlingApplication.java
@@ -0,0 +1,13 @@
+package vaibhav.soni.eventhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vaibhav/soni/eventhandling/EventhandlingApplicationTests.java
@@ -0,0 +1,16 @@
+package vaibhav.soni.eventhandling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventhandlingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
